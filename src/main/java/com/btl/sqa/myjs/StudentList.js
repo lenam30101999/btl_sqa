a3e13cfc55e2b67c290aa506b76b33e7275afd3a
leloi69 <leloi120299@gmail.com>
2021-05-30 21:24:52 +0700

    con ngua cua nam beo

--- src/main/java/com/btl/sqa/myjs/StudentList.js
+++ src/main/java/com/btl/sqa/myjs/StudentList.js
@@ -6,20 +6,20 @@ var ShowStudentList = (function () {
     };
 
     var GetAll = function () {
-        $.ajax({
-            url: 'http://localhost:8080/api/v1/users/getAllStudent',
-            type: "GET",
-            success: function(result) {
-                if(result.length > 0) {
-                    sessionStorage.setItem("Student",JSON.stringify(result));
-                } else {
-                    alert("không có danh sách");
-                }
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        });
+        // $.ajax({
+        //     url: 'http://localhost:8080/api/v1/users/getAllStudent',
+        //     type: "GET",
+        //     success: function(result) {
+        //         if(result.length > 0) {
+        //             sessionStorage.setItem("Student",JSON.stringify(result));
+        //         } else {
+        //             alert("không có danh sách");
+        //         }
+        //     },
+        //     error: function(error) {
+        //         console.log(error);
+        //     }
+        // });
     };
 
     var bindEvent = function () {
