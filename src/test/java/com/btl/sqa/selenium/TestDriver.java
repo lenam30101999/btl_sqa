016ad3f3e3ad1e66290baf0e39e2b1f90d859749
levunam30101999 <levunam30101999@gmail.com>
2021-05-31 16:44:31 +0700

    Add selenium

--- src/test/java/com/btl/sqa/selenium/TestDriver.java
+++ src/test/java/com/btl/sqa/selenium/TestDriver.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestDriver {
 
-  private static final String CHROMEDRIVER_EXE = "chromedriver.exe";
+  private static final String CHROMEDRIVER_EXE = "/Users/macbook/Downloads/chromedriver";
 
   public ChromeDriver getDriver(){
     System.setProperty("webdriver.chrome.driver", CHROMEDRIVER_EXE);
